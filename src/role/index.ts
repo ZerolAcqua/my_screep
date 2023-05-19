c32c936852831aa1613589bfbbb1bacfd81acd4b
Zerol Acqua <hlc118@outlook.com>
2023-05-19 15:41:14 +0800

    update creepController

---
+++ src/role/index.ts
@@ -0,0 +1,17 @@
+// /**
+//  * @todo
+//  */
+
+
+// import baseRoles from './basic'
+// import advancedRoles from './advanced'
+
+
+// const creepWork: CreepWork = {
+//     ...baseRoles,
+//     ...advancedRoles,
+// }
+// /**
+//  * 导出所有的角色
+//  */
+// export default creepWork
